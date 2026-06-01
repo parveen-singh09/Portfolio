829ef4566162bef3a582b8fff03a59405f270e91
parveen125690 <parveen125690@gmail.com>
2026-06-01 10:57:08 +0530

    error color  changes

--- my-app/app/components/Contact.jsx
+++ my-app/app/components/Contact.jsx
@@ -89,8 +89,8 @@ export const Contact = ({ isDarkMode, setIsDarkMode }) => {
         if(result === Success!){
           <p className='mt-4 text-green-500'>{result}</p>
         }
-        else {
-          <p className='mt-4 text-redd-500'>{result}</p>
+        if(result === Error) {
+          <p className='mt-4 text-red-500'>{result}</p>
         }
       </motion.form>
     </motion.div>
